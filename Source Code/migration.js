d3fb76efe30efd8ec4b6d7ea633557994fca66c7
Jainam Barbhaya <jainambarbhaya1509@gmail.com>
2023-06-13 15:02:49 +0530

    updated code to read rows in excel

--- Source Code/migration.js
+++ Source Code/migration.js
@@ -1,27 +1,63 @@
-// Requiring Packages "xlsx" and "pg" from npm and inbuilt "fs" module
-
 const xlsx = require("xlsx");
 const { Pool } = require("pg");
 const fs = require("fs");
 
-// Connecting to the PostgreSQL Server
-
+// PostgreSQL Server Information
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "test",
-    password: "admin",
-    port: 5432
-})
+  user: "postgres",
+  host: "localhost",
+  database: "test",
+  password: "admin",
+  port: 5432
+});
 
-pool.query('SELECT * FROM student')
-  .then((res) => {
-    console.log('Result:', res.rows);
-  })
-  .catch((err) => {
-    console.error('Error executing query', err);
-  })
-  .finally(() => {
-    // Release the pool connection
-    pool.end();
-  });
+// Connect to PostgreSQL Database
+async function connectToPostgreSQL() {
+  try {
+    await pool.connect();
+    console.log('Connected to PostgreSQL');
+  } catch (error) {
+    console.error('Error connecting to PostgreSQL:', error);
+  }
+}
+
+// Disconnect from PostgreSQL Database
+async function disconnectFromPostgreSQL() {
+  try {
+    await pool.end();
+    console.log('Disconnected from PostgreSQL');
+  } catch (error) {
+    console.error('Error disconnecting from PostgreSQL:', error);
+  }
+}
+
+// Read data from excel file
+async function readExcel(filePath) {
+  const workbook = xlsx.readFile(filePath);
+  const sheetNames = workbook.SheetNames;
+
+  for (const sheetName of sheetNames) {
+    const worksheet = workbook.Sheets[sheetName];
+    const range = xlsx.utils.decode_range(worksheet['!ref']);
+
+    const row = [];
+    for (let c = range.s.c; c <= range.e.c; c++) {
+      const cellAddress = xlsx.utils.encode_cell({ r: range.s.r, c });
+      const cellValue = worksheet[cellAddress] ? worksheet[cellAddress].v : '';
+      row.push(cellValue);
+    }
+
+    console.log(`Sheet: ${sheetName}`);
+    console.log(row);
+  }
+}
+
+// Main function
+async function main() {
+  await connectToPostgreSQL();
+  await readExcel("C:/Users/Jainam Barbhaya/Desktop/Migration Package/Files/student.xlsx");
+  await disconnectFromPostgreSQL();
+}
+
+// Call the main function
+main();
